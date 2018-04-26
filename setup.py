9dfe0b3af0e09be511d57cb217b840dc62aa7ee7
Garrett Honke <garretthonke@gmail.com>
2018-04-26 14:30:26 -0400

    diff. strategy for spacy library issue

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
+import os
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        os.system("python3 -m spacy download en")
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        os.system("python3 -m spacy download en")
+        install.run(self)
+
 
 setup(name='nk_croc',
       version='1.0.0',
       description='Character recognition and object classification system.',
       packages=['nk_croc'],
-      install_requires=['Keras >= 2.0.2',
+      install_requires=[
+                        'Keras >= 2.0.2',
                         'scikit-learn >= 0.18.1',
                         'pandas >= 0.19.2',
                         'scipy >= 0.19.0',
@@ -12,6 +31,9 @@ setup(name='nk_croc',
                         'spacy >= 2.0.9',
                         'requests >= 2.18.4',
                         'numpy >= 1.13.3',
-                        'Pillow >= 5.1.0',
-                        'en_core_web_sm']
+                        'Pillow >= 5.1.0'],
+      cmdclass={
+                'develop': PostDevelopCommand,
+                'install': PostInstallCommand,
+               }
       )

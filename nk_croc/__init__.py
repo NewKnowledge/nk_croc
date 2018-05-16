8e22d72b405ab54885f884527c08e704cbaa6614
Garrett Honke <garretthonke@gmail.com>
2018-05-16 11:37:53 -0400

    importing .py issue

--- nk_croc/__init__.py
+++ nk_croc/__init__.py
@@ -1,11 +1,9 @@
 from nk_croc.nk_croc import Croc
-from is_a import isa_dict
-from id_mapping import id_mapping_dict
+from . import is_a
+from . import id_mapping
 
 __version__ = '1.1.0'
 
 __all__ = [
           'Croc'
-          'id_mapping_dict'
-          'isa_dict'
           ]

7a4059682d2c78c1c5cbaec9089098834264ef13
Garrett Honke <garretthonke@gmail.com>
2018-05-16 11:09:00 -0400

    added imports

--- nk_croc/__init__.py
+++ nk_croc/__init__.py
@@ -1,7 +1,11 @@
 from nk_croc.nk_croc import Croc
+import is_a
+import id_mapping
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 __all__ = [
           'Croc'
+          'is_a'
+          'id_mapping'
           ]
